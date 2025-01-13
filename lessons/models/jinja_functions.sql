9a9b1ef68b59a3c209f3267da4f6cc4ce0d0eedc
Darren Miller <darren.miller@ba.com>
2025-01-13 11:34:19 +0000

    remove another unnecessary file

--- lessons/models/jinja_functions.sql
+++
@@ -1,8 +0,0 @@
-{% set columns = adapter.get_columns_in_relation(ref('dim_orders')) %} -- retrieves all the columns in a table
-
--- we could then loop through them
-select {% for column in columns -%}
-	{%- if column.name.startswith('total') %} -- looks for columns if they start with total
-	{{ column.name.lower() }},
-	{%- endif -%}
-{%- endfor %}
